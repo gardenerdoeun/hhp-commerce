8fc841b2e7e35882bafcc74618734e0e2f209a6d
gardenerdoeun <hneu8609@gmail.com>
2025-01-07 01:42:17 +0900

    build: swagger 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.projectlombok:lombok")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.0")
+	annotationProcessor("org.projectlombok:lombok")
 
 	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
