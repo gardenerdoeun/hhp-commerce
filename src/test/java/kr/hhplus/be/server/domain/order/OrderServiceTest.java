52153cb443022372a5d169485cf35cf332ef35c7
gardenerdoeun <hneu8609@gmail.com>
2025-01-17 03:29:42 +0900

    test: 주문 단위 테스트

---
+++ src/test/java/kr/hhplus/be/server/domain/order/OrderServiceTest.java
@@ -0,0 +1,38 @@
+package kr.hhplus.be.server.domain.order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import kr.hhplus.be.server.domain.product.Product;
+import kr.hhplus.be.server.domain.product.ProductService;
+import kr.hhplus.be.server.infrastructure.inMemory.OrderDetailRepository;
+import kr.hhplus.be.server.infrastructure.inMemory.OrderRepository;
+import kr.hhplus.be.server.infrastructure.inMemory.ProductRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void testCreateOrder() {
+        OrderRepository orepo = new OrderRepository();
+        OrderDetailRepository drepo = new OrderDetailRepository();
+        ProductRepository prepo = new ProductRepository();
+        ProductService pservice = new ProductService(prepo);
+        OrderService oservice = new OrderService(orepo, drepo, pservice);
+
+        // 상품/재고 생성
+        Product product = pservice.createProduct("Notebook", 2000, "SELLING", 5);
+
+        // 주문
+        OrderItem item = new OrderItem(product.getProductId(), 2);
+        Order order = oservice.createOrder(1L, Collections.singletonList(item));
+        assertNotNull(order.getOrderId());
+        assertEquals(3, pservice.getStock(product.getProductId()).getStockQuantity());
+
+        // 실제 확인
+        assertEquals(OrderStatus.COMPLETED, order.getOrderStatus());
+    }
+}
